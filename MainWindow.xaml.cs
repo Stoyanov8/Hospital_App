05a634025da3eec0dccc58816e73d6d330f8e8a2
Georgi Stoyanov <Georgi Stoyanov>
2017-04-18 01:23:11 +0300

    Initial commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Hospital.App
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+
+            String userName = txtBxuserName.Text;
+            string pass = passBxPassword.Password;
+            lblfrgtPass.Visibility = Visibility.Hidden;
+            Mouse.OverrideCursor = Cursors.Wait;
+            txtBxuserName.IsEnabled = false;
+            passBxPassword.IsEnabled = false;
+            btnLogin.IsEnabled = false;
+            if (userName == "admin" && pass == "admin")
+            {
+                loadingGif.Visibility = Visibility.Visible;
+                await Task.Delay(3000);
+                Home home = new Home();
+                home.Show();
+                Close();
+                Mouse.OverrideCursor = Cursors.Arrow;
+            }
+            else
+            {
+
+                lblfrgtPass.Visibility = Visibility.Visible;
+                lblfrgtPass.Content = "Forgot Password ?";
+                txtBxuserName.IsEnabled = true;
+                txtBxuserName.BorderBrush = Brushes.Red;
+                passBxPassword.IsEnabled = true;
+                passBxPassword.BorderBrush = Brushes.Red;
+                btnLogin.IsEnabled = true;
+                Mouse.OverrideCursor = null;
+            }
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            App.Current.Shutdown();
+        }
+    }
+}
