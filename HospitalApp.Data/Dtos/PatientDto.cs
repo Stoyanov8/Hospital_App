8ec5cdd40e939d0c6283de6c9280359d9fe67cb1
IvanKrastev1 <vancata1926@gmail.com>
2017-04-18 16:55:56 +0300

    PatientDTO

---
+++ HospitalApp.Data/Dtos/PatientDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace HospitalApp.Data.Dtos
+{
+    public class PatientDto
+    {
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Gender { get; set; }
+
+        public DateTime BirthDay { get; set; }
+
+        public string Adress { get; set; }
+
+        public int PhoneNumber { get; set; }
+
+
+    }
+}
---
+++ HospitalApp.Import/AddPatient.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using HospitalApp.Data;
+using HospitalApp.Data.Dtos;
+using Models;
+using Newtonsoft.Json;
+
+namespace HospitalApp.Import
+{
+    public class AddPatient
+    {
+        public static void AddPatients()
+        {
+            var json = File.ReadAllText("../../../datasets/patiets.json");
+            var patients = JsonConvert.DeserializeObject<IEnumerable<PatientDto>>(json);
+            AddPatient.AddPatients(patients);
+        }
+
+        public static void AddPatients(IEnumerable<PatientDto> patients)
+        {
+            using (var context = new HospitalAppEntities())
+            {
+                foreach (var patient in patients)
+                {
+                    context.Patients.Add(new Patient
+                    {
+                        FirstName = patient.FirstName,
+                        LastName = patient.LastName,
+                        Gender = patient.Gender,
+                        BirthDay = patient.BirthDay,
+                        Adress = patient.Adress,
+                        PhoneNumber = patient.PhoneNumber 
+
+                    });
+
+                }
+                context.SaveChanges();
+            }
+
+        }
+    }
+}
