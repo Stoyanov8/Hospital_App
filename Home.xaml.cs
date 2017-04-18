03895f9aba431246e1134f52cded08d831d2df79
Georgi Stoyanov <g.stoyanov45@gmail.com>
2017-04-18 16:38:02 +0300

    up

--- Home.xaml.cs
+++ Home.xaml.cs
@@ -1,16 +1,12 @@
-﻿using System;
+﻿
+using System.Windows;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+using Hospital.App.Pages;
+using System.Data.SqlClient;
 
 namespace Hospital.App
 {
@@ -22,6 +18,24 @@ namespace Hospital.App
         public Home()
         {
             InitializeComponent();
+            string con = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=master;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            SqlConnection SqlCon = new SqlConnection(con);
+            SqlCon.Open();
+            string FirstName = $"Select FirstName from Doctor Where {Global.password} == Password";
+            string LastName = $"Select LastName from Doctor Where {Global.password} == Password";
+            string Salary = $"Select Salary from Doctor Where {Global.password} == Password";
+            string Gender = $"Select Gender from Doctor Where {Global.password} == Password";
+            string BirthDay = $"Select Birthday from Doctor Where {Global.password} == Password";
+            string type = $"Select Type from Doctor Where {Global.password} == Password";
+            SqlCommand queryCommand = new SqlCommand(FirstName, SqlCon);
+            SqlDataReader queryCommandReader = queryCommand.ExecuteReader();
+            DataTable dataTable = new DataTable();
+            dataTable.Load(queryCommandReader);
+            SqlCon.Close();
+        }
+        private void MyInfo_click(object sender, RoutedEventArgs e)
+        {
+            Main.Content = new MyInfo();
         }
     }
 }
